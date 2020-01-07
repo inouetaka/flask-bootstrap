58dffddf03440c27d516fc13cf8f6ec1a331558c
inouetaka <webroot63@gmail.com>
2020-01-07 10:24:46 +0900

    first commit

---
+++ run.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8888)
